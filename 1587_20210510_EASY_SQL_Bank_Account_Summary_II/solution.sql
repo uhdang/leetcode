99ffa6650cb7d8ff10b72f2196b89118592d86bc
Jung Woo James Jeong <uhdang@gmail.com>
2021-05-10 23:56:05 +0200

    another solution

--- 1587_20210510_EASY_SQL_Bank_Account_Summary_II/solution.sql
+++ 1587_20210510_EASY_SQL_Bank_Account_Summary_II/solution.sql
@@ -1,5 +1,10 @@
+-- Solution 1
 SELECT Users.name, balance
     FROM (
         SELECT account, SUM(amount) AS balance FROM Transactions GROUP BY account
         ) AS B
         LEFT JOIN Users using (account) WHERE balance > 10000;
+
+
+-- Solution #2
+-- SELECT name, SUM(amount) AS balance FROM Transactions LEFT JOIN Users USING (account) GROUP BY 1 HAVING SUM(amount) > 10000;
