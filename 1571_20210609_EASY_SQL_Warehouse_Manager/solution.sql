3d343575e90643f0dad46087fcbf6abec549b330
Jung Woo James Jeong <uhdang@gmail.com>
2021-06-10 00:34:36 +0200

    1571

---
+++ 1571_20210609_EASY_SQL_Warehouse_Manager/solution.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/warehouse-manager/
+
+SELECT a.name AS warehouse_name, SUM(b.volume * a.units) AS volume
+FROM (
+         SELECT product_id, (Width * Length * Height) AS volume FROM Products
+     ) AS b
+         JOIN Warehouse AS a USING (product_id)
+GROUP BY warehouse_name;
