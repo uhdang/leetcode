dd658adb0b85a7ff9c489f54db2e6562c157ef60
Jung Woo James Jeong <uhdang@gmail.com>
2021-06-10 23:13:42 +0200

    1623 - MySQL has its own syntax than just SQL

---
+++ 1623_20210610_EASY_SQL_All_Valid_Triplets_That_Can_Represent_a_Country/solution.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/all-valid-triplets-that-can-represent-a-country/
+SELECT a.student_name AS member_A, b.student_name AS member_B, c.student_name AS member_C
+FROM SchoolA AS a
+         CROSS JOIN SchoolB AS b
+         CROSS JOIN SchoolC AS c
+WHERE a.student_name != b.student_name AND b.student_name != c.student_name AND c.student_name != a.student_name
+    AND a.student_id != b.student_id AND b.student_id != c.student_id AND c.student_id != a.student_id
