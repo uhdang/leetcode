e1b2a61259a6a94fd55b4eb1baca61117130b319
Jung Woo James Jeong <uhdang@gmail.com>
2021-06-12 23:48:40 +0200

    main file for comparison complete

--- 1470_20210612_EASY_CPP_Shuffle_the_Array/Solution.cpp
+++ 1470_20210612_EASY_CPP_Shuffle_the_Array/Solution.cpp
@@ -10,6 +10,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -43,6 +44,27 @@ public:
 };
 
 int main() {
-    cout << 1 << endl;
+
+    Solution solution;
+
+    int input_01_list[] = {2,5,1,3,4,7};
+    std::vector<int> input_01 (input_01_list, input_01_list + sizeof(input_01_list) / sizeof(int) );
+    int n = 3;
+
+    int output_01_list[] = {2,3,5,4,1,7};
+    std::vector<int> output_01 (output_01_list, output_01_list + sizeof(output_01_list) / sizeof(int) );
+
+    vector<int> expected_01 = solution.shuffle(input_01, n);
+
+    cout << "expected_01: ";
+    for (int i = 0; i < expected_01.size(); i++) {
+        cout << expected_01.operator[](i) << " ";
+    }
+
+    cout << "output_01: ";
+    for (int i = 0; i < output_01.size(); i++) {
+        cout << output_01.operator[](i) << " ";
+    }
+
     return 0;
 }
