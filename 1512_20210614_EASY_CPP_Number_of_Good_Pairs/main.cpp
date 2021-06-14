64e4de86870eedf8ef3b93859f1a300b85b1a592
Jung Woo James Jeong <uhdang@gmail.com>
2021-06-15 00:01:53 +0200

    1512 corrected

--- 1512_20210614_EASY_CPP_Number_of_Good_Pairs/main.cpp
+++ 1512_20210614_EASY_CPP_Number_of_Good_Pairs/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main() {
     Solution solution;
     int input[] = {1,2,3,1,1,3};
+    vector<int> input_v (input, input + sizeof(input) / sizeof(int) );
 
-    int output = solution.numIdenticalPairs(input);
+    int output = solution.numIdenticalPairs(input_v);
 
     cout << output << endl;
 
