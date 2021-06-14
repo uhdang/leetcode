a57ae787de6a4567fa5cafa670103ebc2c838260
Jung Woo James Jeong <uhdang@gmail.com>
2021-06-14 23:59:32 +0200

    1512

---
+++ 1512_20210614_EASY_CPP_Number_of_Good_Pairs/Solution.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int numIdenticalPairs(std::vector<int>& nums);
+};
---
+++ 1512_20210614_EASY_CPP_Number_of_Good_Pairs/main.cpp
@@ -0,0 +1,14 @@
+#include "Solution.h"
+
+using namespace std;
+
+int main() {
+    Solution solution;
+    int input[] = {1,2,3,1,1,3};
+
+    int output = solution.numIdenticalPairs(input);
+
+    cout << output << endl;
+
+
+}
